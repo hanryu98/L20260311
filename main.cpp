9300ce1d38ffabc43a668a6cbd904d20f1d1b0ea
hanryu98 <hanryu98@users.noreply.github.com>
2026-03-11 13:22:15 +0900

    수정

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-
 	// 문자열 갯수 세기
 	// 문자열 중에서 특정 문자를 특정문 바꾸기
 	// 문자열 중에서 특정 문자 위치 찾기
-	int StringLength(char*C)
 
+	int StringLength(char*C)
 	{
 		for(int i = 0; ; i++)
 			if (C[i] == '\0')
@@ -16,18 +15,16 @@ using namespace std;
 			}
 	}
 
-
-
 	int main()
-{
+	{
 
-	int A[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	int A [8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-	char Hello[6] = { 'H', 'E', 'L', 'L','O','\0' };
+	char Hello [6] = { 'H', 'E', 'L', 'L','O','\0' };
 	
-	Hello[3] = 'J';
+	Hello[3] = { 'J' };
 
-	char* C= Hello;
+	char* C = Hello;
 
 
 	cout << StringLength(C) << endl;
@@ -36,4 +33,5 @@ using namespace std;
 
 
 	return 0;
-}
+
+	}
